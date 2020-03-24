cf2e12d3b44e69cb71c4765d533ec9242b0d838d
Thomas Chaplin <thomas.chaplin@tray.io>
2020-03-24 22:06:32 +0000

    refactor: 💡 change various configs
    update configs

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,17 +1,17 @@
-export default {
+module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'references-empty': [2, 'never'],
+        // 'references-empty': [2, 'never'],
         'scope-case': [2, 'always', 'lowerCase'],
         'header-max-length': [2, 'always', 100],
     },
 
     parserPreset: {
         parserOpts: {
-            issuePrefixes: ['TEST-', 'FEAT-', 'BUG-'],
+            // issuePrefixes: ['TEST-', 'FEAT-', 'BUG-'],
             headerPattern: /^(\w*)(?:\((.*)\))?: (.*) (.*)$/,
             headerCorrespondence: [`type`, `scope`, `subject`],
-            noteKeywords: [`BREAKING CHANGE`],
+            // noteKeywords: [`BREAKING CHANGE`],
             revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
             revertCorrespondence: [`header`, `hash`],
         },
